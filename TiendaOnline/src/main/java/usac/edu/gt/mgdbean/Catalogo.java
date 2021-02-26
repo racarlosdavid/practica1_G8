bb4959b72343f9ba5921d6b8c0d52e4295371e17
Sebastian <marcoscabrera96@gmail.com>
2021-02-25 23:02:11 -0600

    Se agrego el controlador de la pagina TiendaOnline

---
+++ TiendaOnline/src/main/java/usac/edu/gt/mgdbean/Catalogo.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package usac.edu.gt.mgdbean;
+
+@Named
+@RequestScoped
+public class Catalogo {
+    private String nombreAplicacion;
+    
+    public void init(){
+        nombreAplicacion="Catalogo";
+    }
+    
+    public void destroy(){
+    }
+    
+    public String getNombreAplicacion(){
+        return nombreAplicacion;
+    }
+    public void setNombreAplicacion(String x1){
+        this.nombreAplicacion=x1;
+    }
+}
+

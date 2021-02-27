0800aaa9e1faf6d50469105c4ae92c14bd5ebd81
carlos david <2111724640101@ingenieria.usac.edu.gt>
2021-02-26 21:25:34 -0600

    Se implemento la entidad Categoria y Pago

---
+++ entidades_practica1/src/main/java/usac/edu/gt/entity/Categoria.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package usac.edu.gt.entity;
+
+/**
+ *
+ * @author iDavi
+ */
+@Entity
+public class Categoria {
+    
+    int codigoCategoria;
+    String nombre;
+    String descripcion;
+
+    public int getCodigoCategoria() {
+        return codigoCategoria;
+    }
+
+    public void setCodigoCategoria(int codigoCategoria) {
+        this.codigoCategoria = codigoCategoria;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+    
+    
+    
+}
